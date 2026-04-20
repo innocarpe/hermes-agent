a23bbad105286d5574b9c5a6015b547966de1b20
Zexin Yuan <git@yzx9.xyz>
2026-04-20 09:56:47 +0800

    fix(nix): make working directory writable
    (cherry picked from commit 15efb410d035faf117d584570ca0566b476b01cd)

--- nix/nixosModules.nix
+++ nix/nixosModules.nix
@@ -770,7 +770,10 @@ HERMES_NIX_ENV_EOF
             NoNewPrivileges = true;
             ProtectSystem = "strict";
             ProtectHome = false;
-            ReadWritePaths = [ cfg.stateDir ];
+            ReadWritePaths = [
+              cfg.stateDir
+              cfg.workingDirectory
+            ];
             PrivateTmp = true;
           };
 
